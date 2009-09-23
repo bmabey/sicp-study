e5b841c16d81be039165d6209a6c540bb47682f9
Ben Mabey <ben@benmabey.com>
2009-09-22 22:13:04 -0600

    my clojure version of 1.7

---
+++ clojure/week01/ex1_7.clj
@@ -0,0 +1,35 @@
+(defn square [x]
+  (* x x))
+
+(defn abs [x]
+  (if (< x 0) (- x) x))
+
+(defn average [numbers]
+  (/ (apply + numbers) (count numbers)))
+
+(defn improve [guess x]
+  (average (list guess (/ x guess))))
+
+(defn within-delta? [x y delta]
+  (<= (abs (- x y)) delta))
+
+(defn sqrt-iter [old-guess guess x]
+  (if (within-delta? old-guess guess 0.000001)
+    guess
+    (sqrt-iter guess (improve guess x) x)
+    ))
+
+(defn sqrt [x]
+  (sqrt-iter 0.0 1.0 x))
+
+(sqrt 9)
+
+(sqrt (+ 100 37))
+
+(sqrt (+ (sqrt 2) (sqrt 3)))
+
+(square (sqrt 1000))
+
+(sqrt 0.005)
+(sqrt 123456789012345678901234567890)
+
