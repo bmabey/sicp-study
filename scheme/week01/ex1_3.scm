786ddb396e851f90d67fe3782ce5429d49cabc9a
Ben Mabey <ben@benmabey.com>
2009-09-25 08:00:46 -0600

    using schemeunit on 1.3

--- scheme/week01/ex1_3.scm
+++ scheme/week01/ex1_3.scm
@@ -1,15 +1,18 @@
+#lang scheme/base
+(require (planet schematics/schemeunit:3))
+
 ;; SICP 1.3
 
 ;; Exercise 1.3. Define a procedure that takes three numbers as
 ;; arguments and returns the sum of the squares of the two larger
 ;; numbers.
 
-(define (square x) (* x x))
-(define (squared-sum x y)
-  (+ (square x) (square y))
-  )
 
 (define (squared-sum-of-two-greatest x y z)
+  (define (square x) (* x x))
+  (define (squared-sum x y)
+    (+ (square x) (square y))
+    )
   (if (> x y)
       (if (> y z)
           (squared-sum x y)
@@ -22,9 +25,6 @@
       )
   )
 
-(squared-sum-of-two-greatest 2 1 3)
-; # => 13
-(squared-sum-of-two-greatest 5 5 9)
-; # => 106
-(squared-sum-of-two-greatest 1 3 4)
-; # => 25
+(check-eq? (squared-sum-of-two-greatest 2 1 3) 13)
+(check-eq? (squared-sum-of-two-greatest 5 5 9) 106)
+(check-eq? (squared-sum-of-two-greatest 1 3 4) 25)
