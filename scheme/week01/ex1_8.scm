69480a7a76f922d39a2b5c48f3da332b840f1654
Ben Mabey <ben@benmabey.com>
2009-09-24 22:41:37 -0600

    now using SchemeUnit to verify ex1_8.scm

--- scheme/week01/ex1_8.scm
+++ scheme/week01/ex1_8.scm
@@ -1,9 +1,14 @@
+#lang scheme/base
+(require (planet schematics/schemeunit:3))
+
 ;; Exercise 1.8.  Newton's method for cube roots is based on the fact that if y is an approximation to the
 ;; cube root of x, then a better approximation is given by the value
 ;; ((x / y^2) + 2y) / 3
 ;; Use this formula to implement a cube-root procedure analogous to the square-root procedure. (In
 ;; section 1.3.4 we will see how to implement Newton's method in general as an abstraction of these
 ;; square-root and cube-root procedures.)
+
+
 
 ; square and within-delta? seem reasonably helpful/general so I'm not boxing them in to cbrt
 (define (square x)
@@ -23,5 +28,6 @@
 
   (cbrt-iter 0.0 1.0 x))
 
-
-(cbrt 27)
+(check-= (cbrt 27) 3.0 0.000001)
+(check-= (cbrt 8E-15) 2E-5 1.0)
+(check-= (cbrt 8E15) 2E5 1.0)
