93d3b06f832aead1a0e5f7ba1b2269ff16fbae6a
Ben Mabey <ben@benmabey.com>
2009-10-19 21:07:03 -0600

    exercise 1.43

---
+++ scheme/week03/ex1_43.scm
@@ -0,0 +1,25 @@
+#lang scheme/base
+(require (planet schematics/schemeunit:3))
+; Exercise 1.43.  If f is a numerical function and n is a positive integer, then we can form the nth repeated
+; application of f, which is defined to be the function whose value at x is f(f(...(f(x))...)). For example,
+; if f is the function x -> x + 1, then the nth repeated application of f is the function x -> x + n. If f is the
+; operation of squaring a number, then the nth repeated application of f is the function that raises its
+; argument to the 2nth power. Write a procedure that takes as inputs a procedure that computes f and a
+; positive integer n and returns the procedure that computes the nth repeated application of f. Your
+; procedure should be able to be used as follows:
+; ((repeated square 2) 5)
+; 625
+
+(define (square x) (* x x))
+(define (compose f g) (lambda (x) (f (g x))))
+
+(define (repeated f n)
+ (define (repeat-fn f-prime i)
+  (if (>= i n) f-prime
+   (repeat-fn (compose f f-prime) (+ i 1))))
+ (repeat-fn f 1))
+
+
+(check-eq? ((repeated square 2) 5) 625)
+(check-eq? ((repeated square 0) 2) 4)
+
