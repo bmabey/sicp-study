5dbb5dd07290f7013586ea0b0023a3d64e14acb5
Ben Mabey <ben@benmabey.com>
2010-07-11 13:41:31 -0600

    2.59 and 2.64

--- scheme/week07/ex2_59.scm
+++ scheme/week07/ex2_59.scm
@@ -4,8 +4,9 @@
 ; Implement the union-set operation for the unordered-list representation of sets.
 
 (define (element-of-set? x set)
-  (cond ((null? set) #f)
-        ((equal? x (car set)) #t)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
         (else (element-of-set? x (cdr set)))))
 
 (define (adjoin-set x set)
@@ -14,12 +15,17 @@
       (cons x set)))
 
 (define (intersection-set set1 set2)
-  (cond ((or (null? set1) (null? set2)) '())
-
-        ((element-of-set? (car set1) set2)
-         (cons (car set1)
-               (intersection-set (cdr set1) set2)))
-        (else (intersection-set (cdr set1) set2))))
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (intersection-set (cdr set1)
+                                       (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set set1 (cdr set2)))))))
 
 (define make-set list)
 
